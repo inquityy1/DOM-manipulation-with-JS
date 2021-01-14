504cc561b0b419a3d094094109fe99e5ea7be3c2
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-14 14:39:24 +0100

    stop timers and executions

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,4 +1,8 @@
 
-setInterval(() => {
+const intervalId = setInterval(() => {
 	console.log('sending analytics data');
 }, 2000);
+
+document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+	clearInterval(intervalId);
+});
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -167,7 +167,11 @@ class App {
       activeProjectsList.addProject.bind(activeProjectsList)
     );
 	
-	setTimeout(this.startAnalytics, 3000);
+	const timerId = setTimeout(this.startAnalytics, 3000);
+	
+	document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+		clearTimeout(timerId);
+	});
   }
   
 	static startAnalytics() {
