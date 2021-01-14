797454e5d740c83d2208d2994c53c967a6edfd6b
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-14 14:52:43 +0100

    custom error

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,6 +1,8 @@
 
 const intervalId = setInterval(() => {
-	console.log('sending analytics data');
+	const customError = new Error('Something went wrong!')
+	customError.come = 404;
+	console.dir(customError);
 }, 2000);
 
 document.getElementById('stop-analytics-btn').addEventListener('click', () => {
